6dfe9e72db6e1922ac6a41ac91a2f74212b04ce7
Nacho Iacovino <nacho.iacovino@gmail.com>
2020-10-30 18:51:55 +0100

    Install axios and made basic request

--- src/pages/Pokemon/Pokemon.js
+++ src/pages/Pokemon/Pokemon.js
@@ -1,6 +1,22 @@
 import './Pokemon.scss';
 
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 const Pokemon = () => {
+  const [pokemon, setPokemon] = useState([]);
+  const baseUrl = 'https://pokeapi.co/api/v2/';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(`${baseUrl}pokemon`, {
+        params: { limit: 151 },
+      });
+      console.log(res.data);
+    };
+    fetchData();
+  }, []);
+
   return <div>hi</div>;
 };
 
