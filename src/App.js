76b97b51681efc1ce15c2e6ea276d4fd05514cc2
Nacho Iacovino <nacho.iacovino@gmail.com>
2020-10-30 18:45:23 +0100

    Add routing for detail

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Switch } from 'react-router-dom';
 
 import Pokemon from './pages/Pokemon/Pokemon';
+import PokemonDetail from './pages/PokemonDetail/PokemonDetail';
 
 const App = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Pokemon} />
+      <Route exact path="/pokemon" component={Pokemon} />
+      <Route exact path="/pokemon/:name" component={PokemonDetail} />
     </Switch>
   );
 };
