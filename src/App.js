9d31bf9d04c6605b2d634bf818f0bdf405a62c40
Nacho Iacovino <nacho.iacovino@gmail.com>
2020-10-31 00:33:24 +0100

    Add Redirect from '/'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 import Pokemon from './pages/Pokemon/Pokemon';
 import PokemonDetail from './pages/PokemonDetail/PokemonDetail';
@@ -6,6 +6,7 @@ import PokemonDetail from './pages/PokemonDetail/PokemonDetail';
 const App = () => {
   return (
     <Switch>
+      <Redirect exact from="/" to="/pokemon" />
       <Route exact path="/pokemon" component={Pokemon} />
       <Route exact path="/pokemon/:name" component={PokemonDetail} />
     </Switch>
